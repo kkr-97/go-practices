b1b2a7e9b9944bdf02f6772d1a51b2674f07ae4e
kkr-97 <kollikeerthan07@gmail.com>
2025-01-10 15:04:18 +0530

    `Added withTimeoutExample and routineGo function, modified main.go to use withTimeoutExample`

--- 22context/main.go
+++ 22context/main.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
-func main() {
+func withCancelExample() {
 	ctx, cancel := context.WithCancel(context.Background())
 	var wg sync.WaitGroup
 	defer cancel()
@@ -14,10 +15,10 @@ func main() {
 	wg.Add(1)
 
 	go func(ctx context.Context, wg *sync.WaitGroup) {
-		defer wg.Done()
 		select {
 		case <-ctx.Done():
 			println("This routine cancelled:", ctx.Err())
+			wg.Done()
 		}
 
 	}(ctx, &wg)
@@ -25,4 +26,43 @@ func main() {
 	fmt.Println("main func exited...")
 
 	wg.Wait()
+}
+
+func routineGo(ctx context.Context, wg *sync.WaitGroup, res chan<- int) {
+	defer wg.Done()
+	fmt.Println("go routine started")
+	select {
+
+	case <-time.After(3 * time.Second):
+		fmt.Println("If this prints, ctx is still running")
+	case <-ctx.Done():
+		fmt.Println("Routine timeout")
+		res <- -1
+		return
+	}
+
+	fmt.Println("rest of go routing code")
+	res <- 1
+}
+
+func withTimeOutExample() {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
+	var wg sync.WaitGroup
+	res := make(chan int)
+
+	wg.Add(1)
+	go routineGo(ctx, &wg, res)
+
+	fmt.Println("Result Status:", <-res)
+
+	fmt.Println("End of main func")
+	wg.Wait()
+
+}
+
+func main() {
+	// withCancelExample()
+
+	withTimeOutExample()
+
 }
